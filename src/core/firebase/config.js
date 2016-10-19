54080af677bac973c73fb21e562496a26a1bb476
otwm <otwmoo@gmail.com>
2016-10-20 02:14:06 +0900

    파이어 베이스 기초 코드

---
+++ src/core/firebase/config.js
@@ -0,0 +1,11 @@
+/**
+ * 파이어 베이스 키 정보
+ * @type {{apiKey: string, authDomain: string, databaseURL: string, storageBucket: string, messagingSenderId: string}}
+ */
+export const firebaseConfig = {
+    apiKey: "AIzaSyCs2I3T0CN8lVE427IgiKEfTcZEhUygQAQ",
+    authDomain: "todo-455d3.firebaseapp.com",
+    databaseURL: "https://todo-455d3.firebaseio.com",
+    storageBucket: "todo-455d3.appspot.com",
+    messagingSenderId: "700658183172"
+};
