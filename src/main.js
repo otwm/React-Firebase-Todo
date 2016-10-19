be6710ffc1eb98f09f8fe85e9c3b30a35e3cab88
otwm <otwmoo@gmail.com>
2016-10-19 23:12:37 +0900

    기초 뼈대(store 등, 리듀서 미작성으로 인한 오류는 존재)

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {AppContainer} from "react-hot-loader";
+
+import {initAuth} from './core/auth';
+import configureStore from './core/store';
+import {browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import Root from './views/root';
+
+const store = configureStore();
+const syncedHistory = syncHistoryWithStore(browserHistory,store);
+const rootElement = document.getElementById('root');
+
+function render(Root) {
+    ReactDOM.render(
+        <AppContainer>
+            <Root history={syncedHistory} store={store}/>
+        </AppContainer>
+        ,rootElement
+    );
+}
+
+if(module.hot){
+    module.hot.accept('./views/root',()=>{
+       render(require('./views/root').default);
+    });
+}
+
+initAuth(store.dispatch)
+    .then(() => render(Root))
+    .catch(error => console.error(error));
